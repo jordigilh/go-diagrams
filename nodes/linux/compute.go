b0702bf54fb8876cfaacfe51d35a94b8f93d1586
Jordi Gil <jgil@redhat.com>
2022-08-18 23:39:08 -0400

    Added flotta resource

--- nodes/linux/compute.go
+++ nodes/linux/compute.go
@@ -26,3 +26,8 @@ func (u *unixNode) Buffer(opts ...diagram.NodeOption) *diagram.Node {
 	nopts := diagram.MergeOptionSets(diagram.OptionSet{diagram.Icon("../assets/linux/buffer.png")}, u.opts, opts)
 	return diagram.NewNode(nopts...)
 }
+
+func (u *unixNode) Flotta(opts ...diagram.NodeOption) *diagram.Node {
+	nopts := diagram.MergeOptionSets(diagram.OptionSet{diagram.Icon("../assets/linux/flotta.png")}, u.opts, opts)
+	return diagram.NewNode(nopts...)
+}
