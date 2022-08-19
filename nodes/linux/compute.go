4ef3c4c06b45b10604fdc75702249e2bcd0aabc6
Jordi Gil <jgil@redhat.com>
2022-08-18 23:29:23 -0400

    added buffer and resized podman and systemd to 256x256

--- nodes/linux/compute.go
+++ nodes/linux/compute.go
@@ -21,3 +21,8 @@ func (u *unixNode) Podman(opts ...diagram.NodeOption) *diagram.Node {
 	nopts := diagram.MergeOptionSets(diagram.OptionSet{diagram.Icon("../assets/linux/podman.png")}, u.opts, opts)
 	return diagram.NewNode(nopts...)
 }
+
+func (u *unixNode) Buffer(opts ...diagram.NodeOption) *diagram.Node {
+	nopts := diagram.MergeOptionSets(diagram.OptionSet{diagram.Icon("../assets/linux/buffer.png")}, u.opts, opts)
+	return diagram.NewNode(nopts...)
+}
